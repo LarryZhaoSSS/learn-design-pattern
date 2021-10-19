8e051d87a814690dc9af2d7eb84f39113b520e7a
LarryKameZhao <larryzhao1994@outlook.com>
2021-10-19 17:38:02 +0800

    init webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log(100)
+class Person {
+    constructor(name) {
+        this.name = name
+    }
+    getName() {
+        return this.name
+    }
+}
+let p = new Person("pppp")
+alert(p.getName())
